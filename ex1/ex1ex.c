a1036416ff0609cf055b0b90c2c3846e38028d1c
Robert Zylla <Robert@Roberts-MBP.fritz.box>
2018-09-22 11:13:25 +0200

    ex1 and ex2 completed

---
+++ ex1/ex1ex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+    // Something more complex
+    printf("This is %d complex stuff.\n", 1);
+
+    return 0;
+}
